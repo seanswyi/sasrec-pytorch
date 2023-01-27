85782e69f093813ed4b7ad9b77154c14481546db
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-01-27 15:19:28 +0900

    Main module.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import time
 
 from arguments import get_args
 from dataset import Dataset
+from model.sasrec import SASRec
 
 
 def main() -> None:
@@ -11,6 +12,12 @@ def main() -> None:
     print(args)
 
     dataset = Dataset(args)
+
+    args.num_users = len(dataset.user2items)
+    args.num_items = len(dataset.item2users)
+
+    model = SASRec(args)
+
     import pdb; pdb.set_trace()
 
 
