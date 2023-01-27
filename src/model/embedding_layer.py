8f06d837b760605f4920d992e1069b50099d6329
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-01-27 15:19:49 +0900

    Embedding layer containing item embedding matrix and positional embedding matrix.

---
+++ src/model/embedding_layer.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+
+
+class EmbeddingLayer(nn.Module):
+    def __init__(self,
+                 num_items: int,
+                 hidden_dim: int,
+                 max_seq_len: int) -> None:
+        super().__init__()
+
+        self.item_emb_matrix = nn.Embedding(num_embeddings=num_item,
+                                            embedding_dim=hidden_dim)
+        self.positional_emb = nn.Parameter(data=torch.rand(size=(max_seq_len, hidden_dim)))
+        nn.init.normal_(self.positional_emb)
+
+    def forward(self, x):
+        x = self.item_emb_matrix(x)
+        x += self.positional_emb
+        return x
