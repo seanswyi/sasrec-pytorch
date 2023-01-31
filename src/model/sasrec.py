6bf36360c2204ad0f38731774fa819d792cf114d
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-02-01 08:35:00 +0900

    Implemented separate classifier layer.
    Depending on whether args.share_item_emb is set to true or not,
    we'll have a separate linear layer that acts as a classifier layer.
    
    I'll also have to implement a if-else branch in the forward method.
    I'm also curious if there's a way to simply use one classifier layer
    and copy the weights. Would this be a deep copy? If so, that wouldn't
    work.

--- src/model/sasrec.py
+++ src/model/sasrec.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 
 from .embedding_layer import EmbeddingLayer
-from .self_attn import SelfAttn
+from .self_attn_block import SelfAttnBlock
 
 
 class SASRec(nn.Module):
@@ -13,9 +13,18 @@ class SASRec(nn.Module):
         self.embedding_layer = EmbeddingLayer(num_items=args.num_items,
                                               hidden_dim=args.hidden_dim,
                                               max_seq_len=args.max_seq_len)
-        self.self_attention = SelfAttn(hidden_dim=args.hidden_dim)
+
+        self_attn_blocks = [SelfAttnBlock(hidden_dim=args.hidden_dim,
+                                          dropout_p=args.dropout_p)
+                            for _ in range(args.num_blocks)]
+        self.self_attn_blocks = nn.Sequential(*self_attn_blocks)
+
+        self.share_item_emb = args.share_item_emb
+        if args.share_item_emb:
+            self.classifier = nn.Linear(in_features=args.hidden_dim,
+                                        out_features=args.num_items)
 
     def forward(self, x):
-        x_embedded = self.embedding_layer(x)
-        x_attn = self.self_attention(x_embedded)
+        x_embedded = self.dropout(self.embedding_layer(x))
+        x_attn = self.self_attn_blocks(x_embedded)
         import pdb; pdb.set_trace()
