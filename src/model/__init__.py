ea81053bb1ca21564dc20b8235af400c19997819
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-02-03 16:47:55 +0900

    Added import statement for sasrec.SASRec.

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -3,3 +3,4 @@ from .pointwise_ffnn import PointWiseFFNN
 from .scaled_dotprod_attn import scaled_dotprod_attn
 from .self_attn import SelfAttn
 from .self_attn_block import SelfAttnBlock
+from .sasrec import SASRec
