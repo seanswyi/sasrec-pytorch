8225caa3cfd57d5c7f2617c00db3e2445fe7cc7f
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-02-03 13:49:41 +0900

    Changed args to ModelArgs + imports.

--- src/model/sasrec.py
+++ src/model/sasrec.py
@@ -1,30 +1,30 @@
+import argparse
+
 import torch
 from torch import nn
 
-from .embedding_layer import EmbeddingLayer
-from .self_attn_block import SelfAttnBlock
+from model import EmbeddingLayer, SelfAttnBlock
 
 
 class SASRec(nn.Module):
-    def __init__(self, args) -> None:
+    def __init__(self, model_args: argparse.Namespace) -> None:
         super().__init__()
 
-        self.args = args
-        self.embedding_layer = EmbeddingLayer(num_items=args.num_items,
-                                              hidden_dim=args.hidden_dim,
-                                              max_seq_len=args.max_seq_len)
+        self.embedding_layer = EmbeddingLayer(num_items=model_args.num_items,
+                                              hidden_dim=model_args.hidden_dim,
+                                              max_seq_len=model_args.max_seq_len)
 
-        self_attn_blocks = [SelfAttnBlock(hidden_dim=args.hidden_dim,
-                                          dropout_p=args.dropout_p)
-                            for _ in range(args.num_blocks)]
+        self_attn_blocks = [SelfAttnBlock(hidden_dim=model_args.hidden_dim,
+                                          dropout_p=model_args.dropout_p)
+                            for _ in range(model_args.num_blocks)]
         self.self_attn_blocks = nn.Sequential(*self_attn_blocks)
 
-        self.dropout = nn.Dropout(p=args.dropout_p)
+        self.dropout = nn.Dropout(p=model_args.dropout_p)
 
-        self.share_item_emb = args.share_item_emb
-        if args.share_item_emb:
-            self.classifier = nn.Linear(in_features=args.hidden_dim,
-                                        out_features=args.num_items)
+        self.share_item_emb = model_args.share_item_emb
+        if model_args.share_item_emb:
+            self.classifier = nn.Linear(in_features=model_args.hidden_dim,
+                                        out_features=model_args.num_items)
 
     def forward(self, x):
         x_embedded = self.dropout(self.embedding_layer(x))
