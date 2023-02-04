9a1d695ca132250fc5fd3705a3fff73b6ae3721b
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-02-04 09:59:38 +0900

    New function to get the scheduler.
    As of now there's only OneCycleLR implemented but I assume there will
    be more in the future.

---
+++ src/utils/get_scheduler.py
@@ -0,0 +1,19 @@
+from torch.optim import lr_scheduler, Optimizer
+
+
+def get_scheduler(optimizer: Optimizer,
+                  scheduler_type: str,
+                  num_batches: int,
+                  num_epochs: int,
+                  warmup_ratio: float) -> lr_scheduler:
+    total_steps = int(num_batches * num_epochs)
+
+    if scheduler_type == 'onecycle':
+        scheduler = lr_scheduler.OneCycleLR(optimizer=optimizer,
+                                            total_steps=total_steps,
+                                            pct_start=warmup_ratio,
+                                            anneal_strategy='linear')
+    else:
+        raise NotImplementedError
+
+    return scheduler
