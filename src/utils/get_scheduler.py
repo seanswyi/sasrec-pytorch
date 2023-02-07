15dace26e8288df0995f79e9fa9f571459d2d032
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-02-08 07:32:17 +0900

    Added missing required argument max_lr.
    Added the max_lr argument to the Trainer's kwarg list in order to
    be used when we get the scheduler.

--- src/utils/get_scheduler.py
+++ src/utils/get_scheduler.py
@@ -3,6 +3,7 @@ from torch.optim import lr_scheduler, Optimizer
 
 def get_scheduler(optimizer: Optimizer,
                   scheduler_type: str,
+                  max_lr: float,
                   num_batches: int,
                   num_epochs: int,
                   warmup_ratio: float) -> lr_scheduler:
@@ -10,6 +11,7 @@ def get_scheduler(optimizer: Optimizer,
 
     if scheduler_type == 'onecycle':
         scheduler = lr_scheduler.OneCycleLR(optimizer=optimizer,
+                                            max_lr=max_lr,
                                             total_steps=total_steps,
                                             pct_start=warmup_ratio,
                                             anneal_strategy='linear')
