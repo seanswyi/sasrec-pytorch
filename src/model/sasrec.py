b8a2fed46d3e0e8fe504f57cfd08bc0c4a1123df
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-02-08 20:06:58 +0900

    Implemented the forward method.

--- src/model/sasrec.py
+++ src/model/sasrec.py
@@ -28,11 +28,32 @@ class SASRec(nn.Module):
         self.dropout = nn.Dropout(p=dropout_p)
 
         self.share_item_emb = share_item_emb
-        if share_item_emb:
+        if not share_item_emb:
             self.classifier = nn.Linear(in_features=hidden_dim,
                                         out_features=num_items)
 
-    def forward(self, x):
-        x_embedded = self.dropout(self.embedding_layer(x))
-        x_attn = self.self_attn_blocks(x_embedded)
-        return x_attn
+    def forward(self,
+                input_seqs: torch.Tensor,
+                positive_seqs: torch.Tensor=None,
+                negative_seqs: torch.Tensor=None) -> torch.Tensor:
+        input_emb = self.dropout(self.embedding_layer(input_seqs))
+        input_attn = self.self_attn_blocks(input_emb)
+
+        if not self.share_item_emb:
+            output_logits = self.classifier(input_attn)
+        else:
+            output_logits = input_attn @ self.embedding_layer.item_emb_matrix.weight.transpose(1, 0)
+
+        outputs = (output_logits,)
+
+        if (positive_seqs is not None) and (negative_seqs is not None):
+            positive_emb = self.dropout(self.embedding_layer(positive_seqs))
+            negative_emb = self.dropout(self.embedding_layer(negative_seqs))
+
+            positive_logits = input_attn * positive_emb
+            negative_logits = input_attn * negative_emb
+
+            outputs += positive_logits
+            outputs += negative_logits
+
+        return outputs
