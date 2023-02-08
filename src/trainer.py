c7b7009f318c6d98da970d05b9a48306f22a5d0e
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-02-08 20:06:03 +0900

    Implemented positive/negative sequences batching.
    In the process of finishing the train method. As of now I finished
    implementing the portion of code where positive and negative samples
    are received and fed into the model.
    
    Time to implement the loss function next.

--- src/trainer.py
+++ src/trainer.py
@@ -1,18 +1,21 @@
 import argparse
 
+import numpy as np
 import torch
+from torch import nn
 from torch.optim import Optimizer
 from tqdm import tqdm, trange
 
 from dataset import Dataset
 from model import SASRec
-from utils import get_scheduler
+from utils import get_negative_samples, get_scheduler
 
 class Trainer:
     def __init__(self,
                  dataset: Dataset,
                  model: SASRec,
                  optimizer: Optimizer,
+                 max_lr: float,
                  num_epochs: int,
                  warmup_ratio: float,
                  scheduler_type: str) -> None:
@@ -24,6 +27,9 @@ class Trainer:
         self.valid_dataloader = dataset.get_dataloader(data=self.valid_data, shuffle=False)
         self.test_dataloader = dataset.get_dataloader(data=self.test_data, shuffle=False)
 
+        self.positive2negatives = dataset.positive2negatives
+
+        self.max_lr = max_lr
         self.num_epochs = num_epochs
         self.warmup_ratio = warmup_ratio
         self.scheduler_type = scheduler_type
@@ -32,17 +38,18 @@ class Trainer:
         self.optimizer = optimizer
 
     def train(self) -> None:
-        loss_func = nn.CrossEntropyLoss(ignore_idx=0)
+        # loss_func = nn.CrossEntropyLoss(ignore_idx=0)
         scheduler = get_scheduler(optimizer=self.optimizer,
                                   scheduler_type=self.scheduler_type,
+                                  max_lr=self.max_lr,
                                   num_batches=len(self.train_dataloader),
                                   num_epochs=self.num_epochs,
                                   warmup_ratio=self.warmup_ratio)
 
-        epoch_pbar = trange(iterable=self.num_epochs,
+        epoch_pbar = trange(self.num_epochs,
                             desc="Epochs: ",
                             total=self.num_epochs)
-        for epoch in epoch_bar:
+        for epoch in epoch_pbar:
             self.model.train()
             self.model.zero_grad()
             self.optimizer.zero_grad()
@@ -51,6 +58,16 @@ class Trainer:
                               desc="Training",
                               total=len(self.train_dataloader))
             for batch in train_pbar:
+                positive_seqs = batch.clone()
+
+                batch[:, -1] = 0
+                input_seqs = batch.roll(shifts=1)
+                negative_seqs = get_negative_samples(self.positive2negatives, positive_seqs)
+
+                inputs = {'input_seqs': input_seqs,
+                          'positive_seqs': positive_seqs,
+                          'negative_seqs': negative_seqs}
+                output = self.model(**inputs)
                 import pdb; pdb.set_trace()
 
     def evaluate(self):
