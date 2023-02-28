d692520ba0614e4fb65da87ee192fec0ffc0b023
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-02-28 18:39:07 +0900

    Renamed and deleted data.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import argparse
+from datetime import datetime
+import logging
 import os
 import time
 
@@ -17,9 +19,31 @@ from model import SASRec
 from trainer import Trainer
 
 
+logger = logging.getLogger()
+
+
 def main() -> None:
     args = get_args()
     args.device = get_device()
+
+    time_right_now = time.time()
+    timestamp = datetime.fromtimestamp(timestamp=time_right_now).strftime(format='%m-%d-%Y-%H%M')
+    args.timestamp = timestamp
+
+    log_filename = f"sasrec_lr-{args.learning_rate}_batch-size-{args.batch_size}\
+        _num-epochs-{args.num_epochs}.log"
+    args.log_filename = log_filename
+    if not os.path.exists('../logs'):
+        os.makedirs('../logs', exist_ok=True)
+    args.log_filename = os.path.join('../logs', args.log_filename)
+
+    log_msg_format = '[%(asctime)s - %(levelname)s - %(filename)s: %(lineno)d] %(message)s'
+    handlers = [logging.FileHandler(filename=args.log_filename), logging.StreamHandler()]
+    logging.basicConfig(format=log_msg_format,
+                        level=logging.INFO,
+                        handlers=handlers)
+
+    logger.info("Starting main process...")
 
     if args.debug:
         args.num_epochs = 1
