d8d87fa8221b0d5c958914bf0f86cd88019aed22
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-03-17 11:25:09 +0900

    Fixed problems with training resumptionn.

--- src/main.py
+++ src/main.py
@@ -15,7 +15,6 @@ from utils import (get_args,
                    get_device,
                    get_output_name,
                    DatasetArgs,
-                   DotDict,
                    ModelArgs,
                    OptimizerArgs,
                    TrainerArgs)
@@ -49,15 +48,17 @@ def main() -> None:
             with open(file=args_save_filename) as f:
                 args = json.load(fp=f)
 
-            args = DotDict(args)
+            args = argparse.Namespace(**args)
 
-            args.log_filename = os.path.join(args.log_dir, f'{args.resume_dir}.log')
             args.save_dir = resume_dir
             args.resume_dir = resume_dir
+            args.resume_training = True
+            args.log_filename = f'{args.resume_dir.replace(args.output_dir, args.log_dir)}.log'
         else:
             relevant_files = [f for f in os.listdir(args.output_dir) if data_name in f]
             timestamps = [f.split('_')[-1] for f in relevant_files]
-            timestamp_objs = [datetime.strptime(ts, '%m-%d-%Y-%H%M').timestamp() for ts in timestamps]
+            timestamp_objs = [datetime.strptime(ts, '%m-%d-%Y-%H%M').timestamp()
+                              for ts in timestamps]
 
             most_recent_ts_idx = np.argmax(timestamp_objs)
             resume_dir = relevant_files[most_recent_ts_idx]
@@ -66,28 +67,29 @@ def main() -> None:
             with open(file=args_save_filename) as f:
                 args = json.load(fp=f)
 
-            args = DotDict(args)
+            args = argparse.Namespace(**args)
 
-            args.log_filename = os.path.join(args.log_dir, f'{resume_dir}.log')
             args.save_dir = os.path.join(args.output_dir, resume_dir)
             args.resume_dir = resume_dir
+            args.resume_training = True
+            args.log_filename = os.path.join(args.log_dir, f'{args.resume_dir}.log')
+    else:
+        # Get log file information.
+        if not os.path.exists(args.log_dir):
+            os.makedirs(args.log_dir, exist_ok=True)
+        output_name = get_output_name(args, timestamp)
+        log_filename = f'{output_name}.log'
+        args.log_filename = os.path.join(args.log_dir, log_filename)
 
-    # Get log file information.
-    if not os.path.exists(args.log_dir):
-        os.makedirs(args.log_dir, exist_ok=True)
-    output_name = get_output_name(args, timestamp)
-    log_filename = f'{output_name}.log'
-    args.log_filename = os.path.join(args.log_dir, log_filename)
+        # Create save file.
+        args.save_name = output_name
+        args.save_dir = os.path.join(args.output_dir, output_name)
+        if not os.path.exists(args.save_dir):
+            os.makedirs(args.save_dir, exist_ok=True)
 
-    # Create save file.
-    args.save_name = output_name
-    args.save_dir = os.path.join(args.output_dir, output_name)
-    if not os.path.exists(args.save_dir):
-        os.makedirs(args.save_dir, exist_ok=True)
-
-    args_save_filename = os.path.join(args.save_dir, 'args.json')
-    with open(file=args_save_filename, mode='w') as f:
-        json.dump(obj=vars(args), fp=f, indent=2)
+        args_save_filename = os.path.join(args.save_dir, 'args.json')
+        with open(file=args_save_filename, mode='w') as f:
+            json.dump(obj=vars(args), fp=f, indent=2)
 
     # Logging basic configuration.
     log_msg_format = '[%(asctime)s - %(levelname)s - %(filename)s: %(lineno)d] %(message)s'
@@ -95,8 +97,13 @@ def main() -> None:
     logging.basicConfig(format=log_msg_format,
                         level=logging.INFO,
                         handlers=handlers)
+
+    if args.resume_training:
+        logger.warning("Resuming training from previous run.")
+
     logger.info(f"Starting main process with {data_name}...")
     logger.info(f"Output directory set to {args.save_dir}")
+    logger.info(f"Logging file set to {args.log_filename}")
 
     if args.debug:
         args.num_epochs = 1
@@ -108,7 +115,7 @@ def main() -> None:
     model_args = ModelArgs(args)
     model = SASRec(**vars(model_args))
 
-    for name, param in model.named_parameters():
+    for param in model.parameters():
         try:
             init.xavier_uniform_(param.data)
         except ValueError:
